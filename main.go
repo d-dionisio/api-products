76a3a65233823276a67d31a3ebf0acc2af0761c8
Douglas Dionísio <douglas.sdionisio@gmail.com>
2026-08-14 11:52:18 -0300

    adding function create products

--- main.go
+++ main.go
@@ -17,13 +17,35 @@ var products = []Product{
 	{ID: 2, Name: "Teclado", Price: 150},
 }
 
+var nextID = 3
+
 func listProducts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(products)
 }
 
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	var product Product
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, "JSON inválido", http.StatusBadRequest)
+		return
+	}
+
+	product.ID = nextID
+	nextID++
+
+	products = append(products, product)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(product)
+}
+
 func main() {
 	http.HandleFunc("GET /products", listProducts)
+	http.HandleFunc("POST /products", createProduct)
 
 	fmt.Println("Server is running in port 8000")
 	http.ListenAndServe(":8000", nil)
